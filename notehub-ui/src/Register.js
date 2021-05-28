ed44fdc944b503d3f5b5ea37010681f94d7a706c
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2021-05-28 11:29:16 +0300

    register

---
+++ notehub-ui/src/Register.js
@@ -0,0 +1,44 @@
+import './Register.css';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
+
+function Register() {
+    return (
+        <Card className="card-register">
+            <Card.Body className="p-sm-4">
+                <h1 className="text-center">Register</h1>
+                <Alert variant="danger">
+                    E-mail already exists.
+                </Alert>
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formConfirmPassword">
+                        <Form.Label>Confirm Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+
+            </Card.Body>
+        </Card>
+    );
+}
+
+export default Register;
